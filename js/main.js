96f1ac976f584c0e026e013b4c650a0367433e8e
Tristan LaRoche <laroche11235@gmail.com>
2021-01-16 02:40:23 -0500

    named some of the variables that I'll need

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const timeRemainingEl = document.querySelector('#time');
+const questionEl = document.querySelector("#question");
+const optionsEl = document.querySelectorAll("#option");
+const mainEl = document.querySelector("#main");
+const initialsEl = document.querySelector("#inputInitials")
+
+let timeRemaining = 60;
+let currentQ;
