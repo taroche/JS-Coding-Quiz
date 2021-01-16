a5b56c842ce39612dd707fe1fea3274465912153
Tristan LaRoche <laroche11235@gmail.com>
2021-01-16 02:51:11 -0500

    made arry with 5 questions and answers

--- js/main.js
+++ js/main.js
@@ -6,3 +6,30 @@ const initialsEl = document.querySelector("#inputInitials")
 
 let timeRemaining = 60;
 let currentQ;
+let QnA = [
+    {
+        question:"Q1: JavaScript is a ___ -side programming language.",
+        options:['Client', 'Server', 'Both', 'None'],
+        correctOptn: 'Both'
+    },
+    {
+        question:"Q2: Which of the following will write the message “Hello DataFlair!” in an alert box?",
+        options:['alertBox(“Hello DataFlair!”);', 'alert(Hello DataFlair!);', 'msgAlert(“Hello DataFlair!”);', 'alert(“Hello DataFlair!”);'],
+        correctOptn: 'alert(“Hello DataFlair!”);'
+    },
+    {
+        question:"Q3: How do you find the minimum of x and y using JavaScript?",
+        options:['min(x,y);', 'Math.min(x,y)', 'Math.min(xy)', 'min(xy);'],
+        correctOptn: 'Math.min(x,y)'
+    },
+    {
+        question:"Q4: Which JavaScript label catches all the values, except for the ones specified?",
+        options:['catch', 'label', 'try', 'default'],
+        correctOptn: 'default'
+    },
+    {
+        question:'Q5: Which are the correct "if" statements to execute certain code if “x” is equal to 2?',
+        options:['if(x 2)', 'if(x = 2)', 'f(x === 2)', 'if(x != 2 )'],
+        correctOptn: 'f(x === 2)'
+    },
+]
